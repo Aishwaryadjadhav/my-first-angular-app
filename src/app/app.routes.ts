7fb9cb8e10465784526cba6ee4664de2f11f4cdc
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-03-13 20:42:07 +0530

    Added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CareerComponent } from './career/career.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CourseComponent } from './course/course.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
-    {path:'home', component: HomeComponent}
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    // {path:'home', component: HomeComponent},
+    {path:'career', component: CareerComponent},
+    {path:'aboutus', component: AboutusComponent},
+    {path:'course', component: CourseComponent},
+    {path:'contactus', component: ContactusComponent},
+    { path: '**', redirectTo: '' }
 ];
