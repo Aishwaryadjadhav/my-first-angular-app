569083816f4c5b2180be3f8e7502415cd915d0cf
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-03-26 19:47:34 +0530

    added counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  counter: number = 0;
+
+  increment(){
+    this.counter++;
+  }
+  decrement(){
+    this.counter--;
+  }
+
+  reset(){
+    this.counter=0;
+  }
+}
