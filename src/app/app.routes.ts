e28bb138cc185fe70e05e707d8bfaa917671f154
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-04-01 12:51:05 +0530

    Added routes for 'Employee-List' and 'Counter' components in the routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { CareerComponent } from './career/career.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { CourseComponent } from './course/course.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { CounterComponent } from './counter/counter.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     // {path:'home', component: HomeComponent},
+    {path:'employee-list', component: EmployeeListComponent},
+    {path:'counter', component: CounterComponent},
     {path:'career', component: CareerComponent},
     {path:'aboutus', component: AboutusComponent},
     {path:'course', component: CourseComponent},
