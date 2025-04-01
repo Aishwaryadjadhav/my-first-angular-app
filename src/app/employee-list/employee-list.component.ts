6d69e89c1f8fd953f08db8fed5e72f053bc06403
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2025-04-01 12:39:34 +0530

    Fetched employee data from service and displayed it in component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Employee } from '../model/employee';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from '../service/employee.service';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
 
-}
+  constructor(private employeeService: EmployeeService){}
+
+  employees: Employee[]=[];
+
+  ngOnInit(){
+
+    this.employeeService.getEmployees().subscribe(data => {
+      data.forEach((employee)=>console.log(employee));
+      this.employees= data;
+      })
+    }
+  
+  }
+
+    //  this.employees=[
+    //     {id: 1,name: 'aishu', salary: 50000, department: 'IT'},
+    //     {id: 2,name: 'binu', salary: 30000, department: 'IT'}
+    //  ];
+  // }
+
+
